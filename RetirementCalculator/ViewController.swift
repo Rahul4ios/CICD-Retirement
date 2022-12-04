d0407060cc0b3126089eeec857eaa8377ff500e7
RYTheDev <connect2ry4jobs@gmail.com>
2022-12-04 18:21:42 +0530

    Added code to test Analytics

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtFld1: UITextField!
+    @IBOutlet weak var txtFld2: UITextField!
+    @IBOutlet weak var txtFld3: UITextField!
+    @IBOutlet weak var txtFld4: UITextField!
+    @IBOutlet weak var lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if Crashes.hasCrashedInLastSession{
+            
+            let alertC = UIAlertController(title: "OOPs", message: "Sorry for that last crash. We are working on it.", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            alertC.addAction(okAction)
+            
+            present(alertC, animated: animated)
+        }
+        
+        Analytics.trackEvent("navigated_to_calculator")
+    }
 
-
+    @IBAction func tappedOnBtn(_ sender: Any) {
+        
+        let currentAge = txtFld1.text ?? ""
+        let retirementAge = txtFld2.text ?? ""
+        let params = ["currentAge": currentAge, "retirementAge": retirementAge]
+        
+        Analytics.trackEvent("calculate_retirement_amount", withProperties: params)
+    }
+    
 }
 
